9bfca7d1bc720ca035a9e6f1a1677194c9e058e3
JoelClemente <joel.lopez106@alu.ulpgc.es>
2023-12-03 15:37:06 +0000

    Test added

--- src/main/java/software/ulpgc/bigdata/algebra/matrices/longint/test/MatrixParallelMultiplicationTest.java
+++ src/main/java/software/ulpgc/bigdata/algebra/matrices/longint/test/MatrixParallelMultiplicationTest.java
@@ -17,8 +17,8 @@ public class MatrixParallelMultiplicationTest {
 
     @Test
     public void testMatrixParallelMultiplier() throws InterruptedException, IOException {
-        String fileNameA = "A.mtx";
-        String fileNameB = "B.mtx";
+        String fileNameA = "spaceStation_6.mtx";
+        String fileNameB = "spaceStation_6.mtx";
 
         CoordinateMatrix A = MatrixFileReader.readMatrixFromFile(fileNameA);
         CoordinateMatrix B = MatrixFileReader.readMatrixFromFile(fileNameB);
@@ -31,18 +31,21 @@ public class MatrixParallelMultiplicationTest {
 
         double[][] resultArray = new double[denseA.getNumRows()][denseB.getNumCols()];
 
-        int tileSize = 2;
-
-        System.out.println("Tiles of MatrixA:");
-        printTiles(doubleArrayA, tileSize);
-        System.out.println("Tiles of MatrixB:");
-        printTiles(doubleArrayB, tileSize);
+        int tileSize = 250;
 
         Semaphore semaphore = new Semaphore(1);
+        long startTimeParallel = System.currentTimeMillis();
         MatrixParallelMultiplier parallelMultiplier = new MatrixParallelMultiplier(doubleArrayA, doubleArrayB, resultArray, tileSize, semaphore);
         parallelMultiplier.multiplyInParallel();
+        long endTimeParallel = System.currentTimeMillis();
+        long executionTimeParallel = (endTimeParallel - startTimeParallel);
+        System.out.println("Execution time ParallelMultiplication: " + executionTimeParallel + " ms");
 
+        long startTimeDense = System.currentTimeMillis();
         Matrix sequentialResult = DenseMatrixMultiplication.multiply(denseA, denseB);
+        long endTimeDense = System.currentTimeMillis();
+        long executionTimeDense = (endTimeDense - startTimeDense) / 1000;
+        System.out.println("Execution time DenseMultiplication: " + executionTimeDense + " s");
 
         System.out.println("Result Matrix C:");
         printMatrix(resultArray);
@@ -59,21 +62,6 @@ public class MatrixParallelMultiplicationTest {
                 System.out.print(matrix[i][j] + " ");
             }
             System.out.println();
-        }
-    }
-
-    private void printTiles(double[][] matrix, int tileSize) {
-        for (int i = 0; i < matrix.length; i += tileSize) {
-            for (int j = 0; j < matrix[0].length; j += tileSize) {
-                System.out.println("Tile at (" + i + ", " + j + "):");
-                for (int row = i; row < i + tileSize && row < matrix.length; row++) {
-                    for (int col = j; col < j + tileSize && col < matrix[0].length; col++) {
-                        System.out.print(matrix[row][col] + " ");
-                    }
-                    System.out.println();
-                }
-                System.out.println();
-            }
         }
     }
 }
